d1d5b10aaedbf8bdde989eaf04a67e98984ab667
Gabriel-Lima232 <68028935+Gabriel-Lima232@users.noreply.github.com>
2021-06-28 17:22:48 -0300

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,27 @@
+import sys
+import socket
+
+def prPink(skk): print("\033[95m {}\033[00m" .format(skk))
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+
+prPink(""""                                                         
+ _______  _______  __    _  __    _  _______  ______          _______  __   __ 
+|  _    ||   _   ||  |  | ||  |  | ||       ||    _ |        |       ||  | |  |
+| |_|   ||  |_|  ||   |_| ||   |_| ||    ___||   | ||        |    _  ||  |_|  |
+|       ||       ||       ||       ||   |___ |   |_||_       |   |_| ||       |
+|  _   | |       ||  _    ||  _    ||    ___||    __  | ___  |    ___||_     _|
+| |_|   ||   _   || | |   || | |   ||   |___ |   |  | ||   | |   |      |   |  
+|_______||__| |__||_|  |__||_|  |__||_______||___|  |_||___| |___|      |___|            
+""")
+
+if len(sys.argv) < 2 | len(sys.argv) > 2:
+    prRed("[-] How to use -> python3 10.0.0.10 22")
+else:
+    ip = sys.argv[1]
+    port = sys.argv[2]
+
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysocket.connect((ip,int(port)))
+    banner = mysocket.recv(1024)
+
+    print(banner)
